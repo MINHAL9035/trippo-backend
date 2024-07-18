d1f371f8563cd8463ce920118b4fecade30b167c
MINHAL9035 <mhdminhal44@gmail.com>
2024-07-18 18:18:05 +0530

    new commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    // mongoDb connection
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (ConfigService: ConfigService) => {
+        const uri = ConfigService.get<string>('MONGO_URI');
+        Logger.log(`MongoDB connected to ${uri}`, 'MongooseModule');
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
